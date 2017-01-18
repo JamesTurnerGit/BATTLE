de4039272d9db345edd66523d466d0bcb86e84da
James Turner <mail.jt@gmail.com>
2017-01-18 12:17:29 +0000

    21 test helpers start

--- spec/features_spec.rb
+++ spec/features_spec.rb
@@ -1,9 +1,19 @@
-feature "Enter names" do
-  scenario "Enter names for both players" do
+feature "Enter names - " do
+  scenario "Enter names for both players - " do
     visit '/'
     fill_in 'player1', with: 'Turner'
     fill_in 'player2', with: 'Hooch'
     click_button 'Start Battle!'
     expect(page). to have_content "Turner vs. Hooch"
+  end
+end
+
+feature "viewing hit points - " do
+  scenario "seeing player2's hit points - " do
+    visit '/'
+    fill_in 'player1', with: 'Turner'
+    fill_in 'player2', with: 'Hooch'
+    click_button 'Start Battle!'
+    expect(page). to have_content "Hooch Hp:100"
   end
 end
